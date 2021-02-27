71ecd6a10bd44295d2e02895c95450eae392ba2e
jeanbacan <mr.jeanmarcel@gmail.com>
2021-02-27 08:40:05 -0700

    Update BubbleSort.java
    Com print no final.

--- src/main/java/com/algorithm/sorting/BubbleSort.java
+++ src/main/java/com/algorithm/sorting/BubbleSort.java
@@ -1,4 +1,21 @@
 package com.algorithm.sorting;
 
 public class BubbleSort {
+    public static void main(String[] args) {
+
+        sort(new int[]{2,1,4,3,8,7,9,5,6});
+    }
+
+    public static void sort(int[] arr){
+        for (int i=0; i < arr.length; i++){
+            for (int y=0; y < (arr.length - i) - 1; y++){
+                if (arr[y] > arr[y+1]){
+                    int aux = arr[y];
+                    arr[y] = arr[y+1];
+                    arr[y+1] = aux;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
