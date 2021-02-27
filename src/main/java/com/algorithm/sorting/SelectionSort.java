16ba13d33ce216c418a12140534d5d6950bec3d1
Douglas Melo <douglas.melo@skipthedishes.ca>
2021-02-27 09:35:39 -0600

    dsaa-0227-2 - Selection sort

--- src/main/java/com/algorithm/sorting/SelectionSort.java
+++ src/main/java/com/algorithm/sorting/SelectionSort.java
@@ -2,19 +2,20 @@ package com.algorithm.sorting;
 
 public class SelectionSort {
 
-    public static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++)
+    public static int[] selectionSort(int[] array){
+        for (int i = 0; i < array.length - 1; i++)
         {
             int index = i;
-            for (int j = i + 1; j < arr.length; j++){
-                if (arr[j] < arr[index]){
+            for (int j = i + 1; j < array.length; j++){
+                if (array[j] < array[index]){
                     index = j;//searching for lowest index
                 }
             }
-            int smallerNumber = arr[index];
-            arr[index] = arr[i];
-            arr[i] = smallerNumber;
+            int smallerNumber = array[index];
+            array[index] = array[i];
+            array[i] = smallerNumber;
         }
+        return array;
     }
 
 }
