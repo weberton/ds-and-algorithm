9909122398863a4a88987af4edfe1d99d4e2b3a5
Ribamar Santos <ribamarsantos@Ribamars-MacBook-Pro.local>
2021-02-27 09:51:56 -0600

    buble

--- src/main/java/com/algorithm/sorting/BubbleSort.java
+++ src/main/java/com/algorithm/sorting/BubbleSort.java
@@ -1,4 +1,25 @@
 package com.algorithm.sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+
+
+    public static void main(String args []) {
+        int [] myArray = new int [] { 7,5,3,2,1};
+        int aux = 0;
+
+        for (int j = 0; j < myArray.length; j++) {
+            for (int i = 0; i < myArray.length -1; i++) {
+                if (myArray[i] > myArray[i + 1]) {
+                    aux = myArray[i];
+                    myArray[i] = myArray[i + 1];
+                    myArray[i + 1] = aux;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(myArray));
+    }
+
 }
